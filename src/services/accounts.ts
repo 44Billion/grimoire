cf27bceea8307b58f712f4268b65a41503ce4ed8
arthurfranca <arthur.a.franca@gmail.com>
2026-02-08 01:04:56 -0300

    nappify

--- src/services/accounts.ts
+++ src/services/accounts.ts
@@ -1,6 +1,9 @@
 import { AccountManager } from "applesauce-accounts";
-import { registerCommonAccountTypes } from "applesauce-accounts/accounts";
-import { NostrConnectSigner } from "applesauce-signers";
+import {
+  registerCommonAccountTypes,
+  ExtensionAccount,
+} from "applesauce-accounts/accounts";
+import { ExtensionSigner, NostrConnectSigner } from "applesauce-signers";
 import pool from "./relay-pool";
 
 const ACCOUNTS = "nostr-accounts";
@@ -47,5 +50,38 @@ accountManager.active$.subscribe((account) => {
   if (account) localStorage.setItem(ACTIVE_ACCOUNT, account.id);
   else localStorage.removeItem(ACTIVE_ACCOUNT);
 });
+
+// Auto-login with NIP-07 peekPublicKey
+async function checkAutoLogin() {
+  const nostr = (window as any).nostr;
+  if (nostr?.peekPublicKey) {
+    try {
+      const pubkey = await nostr.peekPublicKey();
+      if (pubkey && typeof pubkey === "string") {
+        // Find if we already have this account
+        let account = accountManager.accounts.find(
+          (a) => a.pubkey === pubkey && a.type === "extension",
+        );
+
+        if (!account) {
+          // If not, create it
+          account = new ExtensionAccount(pubkey, new ExtensionSigner());
+          accountManager.addAccount(account);
+        }
+
+        if (accountManager.active?.id !== account.id) {
+          accountManager.setActive(account);
+          console.log(
+            `[Auto-Login] Active account set to ${pubkey} via peekPublicKey`,
+          );
+        }
+      }
+    } catch (err) {
+      // Ignore errors from peekPublicKey
+    }
+  }
+}
+
+checkAutoLogin();
 
 export default accountManager;
